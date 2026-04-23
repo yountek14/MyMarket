f79315090a56d214b1d43d87bbd6f968d7f43d1d
Shir3n <Ignacio.salazar.lopez@gmail.com>
2026-04-23 09:14:16 -0400

    Primer Commit I

---
+++ ms-ususarios/src/main/java/com/example/ms_ususarios/MsUsusariosApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_ususarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUsusariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUsusariosApplication.class, args);
+	}
+
+}
